22ac491d1d24ad28660e88f0801cdff72f332d19
saspero90 <saspero90@gmail.com>
2016-06-13 18:16:13 +0200

    Config de perfil

--- public/scripts/views/user/profile.js
+++ public/scripts/views/user/profile.js
@@ -12,6 +12,7 @@ define([
     var Profile = G.Backbone.View.extend({
         template: G._.template(t_profile),
         temp: G._.template(t_userProfile),
+        tempConfigProfile: G._.template(t_configProfile),
         className: 'container',
 
         initialize: function(){
@@ -28,6 +29,7 @@ define([
         loadPerfil: function(){
             //posa al panell dret l'informació del perfil
             var $rightPanel = this.$el.find('#contingutPerfil');
+            //$rightPanel.html(t_userProfile);
             $rightPanel.html(this.temp({name: userData.username, email: userData.email}));
         },
 
@@ -35,11 +37,26 @@ define([
             //Carregar generalConfiguration
             //es carrega al mateix div (substitució)
             var $rightPanel = this.$el.find('#contingutPerfil');
-            $rightPanel.html(t_configProfile);
-
+            $rightPanel.html(this.tempConfigProfile);//.then({InputUserName: userData.username, InputEmail:userData.email});
+            this.$el.find('#InputUserName').val(userData.username);
+            this.$el.find('#InputEmail').val(userData.email);
         },
 
         saveChanges: function(){
+            //var id = userData.id;
+            var user = new m_user({id:userData.id});
+            user.fetch({
+                success:function(user){
+                    user.save({name:this.$el.find('#InputUserName').val(),email:this.$el.find('#InputEmail').val()},{
+                        success:function(model){
+                            alert("Dades actualitzades");
+                        },
+                        error:function(model){
+                            alert("Error al actualitzar les dades");
+                        }
+                    });
+                }
+            })
             alert("clickat save")
         },
 
